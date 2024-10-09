fca4dbafa7fbde6d2bb48dd130eba307fc44e924
Andrea Vecchione <andrea.vecchione02@gmail.com>
2024-10-09 15:58:11 +0200

    EsLogging

---
+++ src/main/java/com/example/EsLogging/EsLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.EsLogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EsLogging/EsLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EsLogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
